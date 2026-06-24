dc1306003c620082261ac64d69ca04bdece02b0e
Hasini Reddy <hasinireddy6827@gmail.com>
2026-06-24 16:21:20 +0530

    Update deployment configuration

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ app.config['MYSQL_HOST'] = os.environ.get('MYSQLHOST')
 app.config['MYSQL_USER'] = os.environ.get('MYSQLUSER')
 app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQLPASSWORD')
 app.config['MYSQL_DB'] = os.environ.get('MYSQLDATABASE')
+app.config['MYSQL_PORT'] = int(os.environ.get('MYSQLPORT', 3306))
 
 mysql = MySQL(app)
 
